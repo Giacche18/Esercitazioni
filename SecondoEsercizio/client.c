8a4e0cd91de4cf0f3164527b590be0a281b729f1
Giacche <giacchetti125@gmail.com>
2019-09-23 16:55:32 +0200

    File client.c modificato

---
+++ SecondoEsercizio/client.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "socket-lib.h"
+
+#define HOST "www.example.org"
+#define PORT 80
+
+int main() {
+    if(socket_init() != 0) {
+        fprintf(stderr, "Socket initialization failed\n");
+        return 1;
+    }
+
+    SOCKET_T socket = socket_create();
+    if(socket == 0) {
+        fprintf(stderr, "Failed to create socket\n");
+        return 1;
+    }
+
+    if(socket_connect(socket, HOST, PORT) < 0) {
+        fprintf(stderr, "Failed to connect socket\n");
+        return 1;
+    }
+
+    printf("Connected!\n");
+
+    // Read user input
+    char input[1024] = 
+    	"GET / HTTP/1.0\r\n"
+    	"Host: www.example.org\r\n\r\n";
+    	
+    	
+    send(socket, input, strlen(input) * sizeof(char), 0);
+    socket_close_send(socket);
+
+    char output[256];
+    socket_read_all(socket, output, 1024);
+    printf("%s\n", output);
+
+    if(socket_quit() != 0) {
+        fprintf(stderr, "Socket finalization failed\n");
+        return 1;
+    }
+
+    return 0;
+}
